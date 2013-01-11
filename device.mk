455cdb02c457525a1aeed1ad2318361130bef99c
something15525 <something15525@gmail.com>
2013-01-11 12:57:58 -0800

    typo in apn conf

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES := \
 
 PRODUCT_COPY_FILES := \
     device/asus/tilapia/init.tilapia.rc:root/init.grouper.rc \
-    device/asus/tilapia/apns-conf.xml:etc/apns-conf.xml
+    device/asus/tilapia/apns-conf.xml:system/etc/apns-conf.xml
 
 DEVICE_PACKAGE_OVERLAYS := \
     device/asus/tilapia/overlay
